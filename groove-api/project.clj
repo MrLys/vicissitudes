a7ceac4512c86bd3ee6b44fa305039931d6dcfd0
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2019-11-30 23:25:47 +0100

    Add db and remove cors avoidance

--- groove-api/project.clj
+++ groove-api/project.clj
@@ -11,7 +11,16 @@
                  [jumblerg/ring-cors "2.0.0"]
                  [org.clojure/data.json "0.2.7"]
                  [ring/ring-json "0.5.0"]
-                 [ring "1.7.1"]]
+                 [ring "1.7.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-anti-forgery "1.2.0"]
+                 [clj-http "3.10.0"]
+                 [toucan "1.1.9"]
+                 [clj-time "0.15.2"]
+                 [org.postgresql/postgresql "42.2.4"]
+                 [buddy/buddy-hashers "1.4.0"]
+                 [buddy/buddy-auth "2.2.0"]
+                 [ring/ring-ssl "0.3.0"]]
   :main ^:skip-aot groove-api.core
   :target-path "target/%s"
   :profiles {:dev {:main groove-api.core/-dev-main}
--- groove-api/src/groove_api/core.clj
+++ groove-api/src/groove_api/core.clj
@@ -1,19 +1,31 @@
 (ns groove-api.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [compojure.api.sweet :refer [api context routes]]
-            [jumblerg.middleware.cors :refer [wrap-cors]]
-            [groove-api.choice :refer [choice-routes]]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [groove-api.groove :refer [groove-routes]]
+            [groove-api.user :refer [user-routes]]
+            [groove-api.habit :refer [habit-routes]]
+            [groove-api.middleware :refer [wrap-session-auth]]
+            [toucan.db :as db]
+            [toucan.models :as models]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [buddy.auth.accessrules :refer [restrict]]
+            [buddy.auth :refer [authenticated?]]))
 
+(def db-spec
+  {:dbtype "postgres"
+   :dbname "groove-api"
+   :user "lyseggen"
+   :password "groove-api"})
 (def swagger-config
-  {:ui "/swagger"
-   :spec "/swagger.json"
+  {:ui "/api/swagger"
+   :spec "/api/swagger.json"
    :options {:ui {:validatorUrl nil}
              :data {:info {:version "1.0.0", :title "RESTful groove API"}}}})
 (def app 
   (api
     {:swagger swagger-config}
-  (context "/api" [] choice-routes) #".*"))
+  (context "/api" [] groove-routes user-routes habit-routes)))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -22,4 +34,6 @@
 
 (defn -dev-main
   [& args]
-  (run-jetty (wrap-reload #'app) {:port 3000}))
+  (db/set-default-db-connection! db-spec)
+  (models/set-root-namespace! 'groove-api.models)
+  (run-jetty (wrap-reload (wrap-defaults #'app site-defaults)) {:port 3000}))
