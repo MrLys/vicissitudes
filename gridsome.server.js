7d760a7c37918ab4778fe68842b1950f7a4a85b9
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2019-12-07 11:02:48 +0100

    Remove unused and unwanted code
        The server should never fetch data from the backend at startup,
        this does not make sense

--- gridsome.server.js
+++ gridsome.server.js
@@ -19,21 +19,6 @@ module.exports = function (api) {
         })
         );
     });
-  api.loadSource(async actions => {
-    const { data } = await axios.get('http://localhost:3000/api/choices');
-    
-    const collection = actions.addCollection('Choices')
-    const keys = Object.keys(data);
-      console.log(keys);
-      console.log(data);
-    for(const key in keys) {
-
-      collection.addNode({
-        day: keys[key],
-        choice: data[keys[key]]
-      });
-    }
-  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
