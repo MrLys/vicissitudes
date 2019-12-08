9aa68dbc42fdaa0f222bb9626137cdc9fca4b623
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2019-12-08 17:38:10 +0100

    Add parseLong and convert-date func
        parseLong should be rewritten to throw exception when not
        parsable

--- groove-api/src/groove_api/util/utils.clj
+++ groove-api/src/groove_api/util/utils.clj
@@ -6,5 +6,14 @@
 (defn format-groove [groove]
   (assoc groove :state (.toLowerCase (:state groove))))
 
-(defn convert-date [groove]
-  (assoc groove :date (java.sql.Timestamp. (.getTime (:date groove)))))
+(defn convert-date [date]
+  (java.sql.Timestamp. (c/to-long date)))
+(defn convert-date-groove [groove]
+  (assoc groove :date (convert-date (:date groove))))
+
+(defn parseLong [s]
+  (if (instance? Long s)
+    s
+  (if (re-matches #"\d+" s)
+    (Integer/parseInt s)
+    0)))
