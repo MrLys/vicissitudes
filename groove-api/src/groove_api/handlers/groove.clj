390a1c49198a299ea6f2458e7ce698a758c7e4bd
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2019-12-14 20:11:22 +0100

    Move communication with db out of handler

--- groove-api/src/groove_api/handlers/groove.clj
+++ groove-api/src/groove_api/handlers/groove.clj
@@ -2,23 +2,18 @@
   (:require [groove-api.util.validation :refer :all]
             [groove-api.util.utils :refer :all]
             [groove-api.auth.token :refer [create-token]]
-            [groove-api.db :refer :all]
-            [toucan.db :as db]
             [ring.util.http-response :refer [ok unauthorized not-found]]
-            [groove-api.bulwark :refer [get-by-dates]]
+            [groove-api.bulwark :refer [get-by-dates update-groove]]
             [groove-api.models.groove :refer [Groove]]))
 
-(defn create-groove-handler [req]
+(defn update-groove-handler [req]
   (println req)
-  (id->created (db/insert! Groove (format-groove req))))
+  (update-groove req))
 
 (defn get-grooves-handler [request user habit start end]
   (get-by-dates request user habit start end))
 
-(defn update-groove-handler [id req]
-   (if (db/update! Groove req)
-     (ok)
-     (create-groove-handler req)))
+(defn create-groove-handler [groove])
 
 (defn groove->response [day]
   (if day
