2fc755132697f0771855049f2433855430563583
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2019-12-15 19:45:20 +0100

    Add mail test functionality

---
+++ groove-api/src/groove_api/mail_test.clj
@@ -0,0 +1,32 @@
+(ns groove-api.mail-test)
+;; Credit must be given where credit is due!
+;; This is shamelessly taken from https://nakkaya.com/2009/11/10/using-java-mail-api-from-clojure/
+(defn mail [& m]
+  (let [mail (apply hash-map m)
+        props (java.util.Properties.)]
+
+    (doto props
+      (.setProperty "mail.smtp.host" (str (:host mail)))
+      (.setProperty "mail.smtp.port" (str (:port mail))))
+
+      (if (= (:ssl mail) true)
+        (doto props
+          (.setProperty "mail.smtp.socketFactory.class" 
+                        "javax.net.ssl.SSLSocketFactory")
+          (.setProperty "mail.smtp.socketFactory.fallback" "false")
+          (.setProperty "mail.smtp.socketFactory.port" (str (:port mail)))
+          (.put "mail.smtp.starttls.enable" "true")))
+
+      (let  [session (javax.mail.Session/getDefaultInstance props)
+             msg     (javax.mail.internet.MimeMessage. session)
+             recipients (reduce #(str % "," %2) (:to mail))]
+
+        (.setFrom msg (javax.mail.internet.InternetAddress. (str (:from mail))))
+
+        (.addRecipient msg 
+                        (javax.mail.Message$RecipientType/TO)
+                        (javax.mail.internet.InternetAddress. (str (:to mail))))
+
+        (.setSubject msg (:subject mail))
+        (.setText msg (:text mail))
+        (javax.mail.Transport/send msg))))
