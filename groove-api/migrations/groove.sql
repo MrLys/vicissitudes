4c1fda8e9aad1621b10256627398c6f23e959d98
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2019-12-16 22:06:13 +0100

    Use TIMESTAMP instead of date to get time
        Investigate if this includes timezones

--- groove-api/migrations/groove.sql
+++ groove-api/migrations/groove.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "groove" (
     id BIGSERIAL PRIMARY KEY,
     state VARCHAR(10),
     habit_id INTEGER REFERENCES public.user_habit(id) ON DELETE CASCADE,
-    date DATE NOT NULL,
+    date TIMESTAMP NOT NULL,
     owner_id INTEGER REFERENCES public.user(id) ON DELETE CASCADE,
     CONSTRAINT unq_habit_date_owner UNIQUE (habit_id, date, owner_id)
 );
