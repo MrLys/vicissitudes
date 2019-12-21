61e3d68f6af038cc3b83d16820ec05983a09d2aa
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2019-12-22 00:18:21 +0100

    Add activation, waiting and profile page

---
+++ src/pages/Activation.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+  <div class="container mt-16 text-center">
+    <div class="rounded px-8 py-4 bg-glitter-light border-2">
+      <p v-if="loading">Please wait while we activate your account! </p>
+  </div>
+  </div>
+  </Layout>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      feedback: "",
+      positive_feedback: true,
+      loading: true,
+    }
+  },
+  methods: {
+    activate: function () {
+      let token = this.$route.query.token;
+      console.log(token);
+      if(token !== undefined) {
+        let param = 'activation_token='+token;
+        this.$http
+          .post('/api/activate?' + param)
+          .then(response => {
+            this.$router.push('/login');
+          }).catch((error) => {console.log(error); this.loading = false;});
+      } else {
+        this.loading = false;
+      }
+    }
+  },
+  mounted () {
+    this.activate();
+  }
+}
+</script>
