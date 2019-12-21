36a249e7b08916bd81be013b36ea34a0a4413db8
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2019-12-21 15:37:24 +0100

    Update default layout to use new icon
    also use the new colors

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container mx-auto p-6 bg-white rounded-lg shadow-xl">
+  <div class="container mx-auto rounded-lg shadow-xl">
+    <div class="container my-4 mx-auto p-6" >
     <header class="header flex-shrink-0">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
+    <icon style="height:150px" v-on:click="toHome()"/>
       <nav class="nav">
-        <g-link class="ml-2" to="/">Home</g-link>
-        <a v-if="loggedIn" class="ml-2 cursor-pointer" v-on:click="logout()">Logout</a>
-        <a v-if="loggedIn" class="ml-2 cursor-pointer" v-on:click="toHabits()">Habits</a>
-        <g-link class="ml-2" to="/about/">About</g-link>
+        <g-link class="ml-2 text-violet" to="/">Home</g-link>
+        <a v-if="loggedIn" class="ml-2 cursor-pointer text-violet" v-on:click="logout()">Logout</a>
+        <a v-if="loggedIn" class="ml-2 cursor-pointer text-violet" v-on:click="toHabits()">Habits</a>
+        <g-link class="ml-2 text-violet" to="/about/">About</g-link>
       </nav>
     </header>
     <slot class="container mx-auto"/>
+  </div>
+  <div class="pt-16 ">
+  <footer class="w-full text-center border-t border-grey p-2 pin-b bg-glitter-light">
+   <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> 
+  </footer>
+  </div>
   </div>
 </template>
 
@@ -23,10 +28,14 @@ query {
 }
 </static-query>
 <script>
+import icon from '~/assets/svgs/default.svg'
+/* <--g-image src="~/default.svg" alt="Rutta" width="100"/>*/
 export default {
   metaInfo: {
     title: 'Welcome!'
-  },
+  }, components: {
+     icon 
+    },
   data () {
     return {loggedIn: this.$store.getters.isLoggedIn}
   },
@@ -42,6 +51,12 @@ export default {
         console.log("visiting habits");
         this.$router.push('/habits')
       }
+    },
+    toHome: function () {
+      if (this.$router.history.current.path !== '/'){
+        console.log("visiting home");
+        this.$router.push('/')
+      }
     }
   }
 }
@@ -54,6 +69,9 @@ body {
   line-height: 1.5;
 }
 
+card {
+  background-color: #1D4934;
+}
 
 .header {
   display: flex;
