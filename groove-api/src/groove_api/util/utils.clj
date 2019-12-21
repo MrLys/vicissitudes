c7b01d12621c1cf0a69270f7c9ff48520f725695
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2019-12-22 00:14:43 +0100

    Add urlencoding and fix parse problem for port

--- groove-api/src/groove_api/util/utils.clj
+++ groove-api/src/groove_api/util/utils.clj
@@ -1,23 +1,34 @@
 (ns groove-api.util.utils
   (:require [clj-time.format :as f]
             [clj-time.core :as t]
+            [ring.util.codec :as codec]
             [clj-time.coerce :as c]))
+            
 
 
 (defn convert-date [date]
   (if (instance? Long date)
-    (java.sql.Timestamp. date)
-  (java.sql.Timestamp. (c/to-long date))))
+    (java.sql.Timestamp. date))
+  (java.sql.Timestamp. (c/to-long date)))
 
 (defn convert-date-groove [groove]
   (assoc groove :date (convert-date (:date groove))))
 
 (defn format-groove [groove]
   (assoc groove :state (.toLowerCase (:state groove)) :date (convert-date (:date groove))))
+
 (defn parseLong [s]
   (println (str "this cannot be nil " s))
   (if (instance? Long s)
     s
-  (if (re-matches #"\d+" s)
-    (Long/valueOf s)
-    0)))
+    (if (re-matches #"\d+" s)
+      (Long/valueOf s)
+      (throw (.IllegalArgumentException (str s " is not a valid Long"))))))
+       
+
+(defn sanitize [m & args]
+  "Updates each value in a map with the url-encoded variation"
+  (println m)
+  (reduce (fn [r [k v]] (assoc r k (apply codec/url-encode v args))) {} m))
+
+
