c4c58667d810d12e32879d849a7fed6ec69ad40d
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2019-12-23 18:42:47 +0100

    Add test profile to profiles

---
+++ groove-api/profiles.clj
@@ -0,0 +1,18 @@
+{:profiles/dev
+  {:env
+   {:dbtype "postgres"
+    :dbname "groove_api"
+    :user   "Thomas"
+    :password "password"
+    :secret "password"
+    :port 3000}}
+  :profiles/test
+  {:env
+   {:dbtype "postgres"
+    :dbname "groove_api"
+    :user   "Thomas"
+    :password "password"
+    :secret "password"
+    :port 3001}}}
+
+
