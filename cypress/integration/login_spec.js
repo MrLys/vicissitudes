3e9f58bb384e71d03f4c7a9cdadcc520311c2845
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2019-12-26 00:57:19 +0100

    Update tests to work properly.

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -6,6 +6,10 @@ describe('My First Test', function () {
         cy.contains('Home')
         cy.contains('Register')
         cy.contains('Log in').click()
+
+        cy.server()
+        cy.route('POST', '/api/habit').as('new-habit')
+
         cy.url().should('include', '/login')
         var login = 'e8ms2cu1fese7h8k6fcog@test.no'
         var pwd = 'e8ms2cu1fese7h8k6fcog'
@@ -24,24 +28,28 @@ describe('My First Test', function () {
         var rand = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
         cy.get('#habit-field')
             .type(rand+'{enter}')
+        cy.wait('@new-habit').should('have.property', 'status', 200)
         cy.contains(rand)
+
         // wait for the page to refresh
-        cy.wait(500)
         // try to add the same habit again
         cy.get('#add-habit').click()
         cy.get('#habit-field')
-            .type(rand+'{enter}')
+            .type(rand)
         cy.contains(rand)
+        cy.get('#save-habit').click()
 
         // wait for the page to refresh
-        cy.wait(500)
+        cy.wait('@new-habit').should('have.property', 'status', 409)
+
         // add Exercising
         cy.get('#add-habit').click()
-        cy.get('#habit-field')
-            .type('Exercising{enter}')
+        // Not cleared when already exists.
+        cy.get('#habit-field').type('hi').clear()
 
+        cy.get('#habit-field').type('Exercising{enter}').clear();
         // wait for the page to refresh
-        cy.wait(500)
+        cy.wait('@new-habit').should('have.property', 'status', 200)
 
         cy.contains('Exercising')
 
