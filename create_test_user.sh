5a9080b921e34272e4397c737138931dd2e76803
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2020-02-14 15:14:52 +0100

    Add a second user to create test user script
        Will be used to verify that a user does not get others grooves

--- create_test_user.sh
+++ create_test_user.sh
@@ -5,4 +5,13 @@ curl -X POST --header 'Content-Type: application/json' --header 'Accept: applica
    "password": "e8ms2cu1fese7h8k6fcog"  
  }' 'http://localhost:3000/api/register'
 
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
+   "username": "5xj5g8gbt9gs9tp527znb8@test.no", 
+   "email": "5xj5g8gbt9gs9tp527znb8@test.no", 
+   "password": "5xj5g8gbt9gs9tp527znb8"  
+ }' 'http://localhost:3000/api/register'
+
 psql -h localhost -U $DBUSER -d $DBNAME -c "update public.user set activated = 't' where username = 'e8ms2cu1fese7h8k6fcog@test.no';"
+
+psql -h localhost -U $DBUSER -d $DBNAME -c "update public.user set activated = 't' where username = '5xj5g8gbt9gs9tp527znb8@test.no';"
+
