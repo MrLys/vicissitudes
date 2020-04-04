ddde4eebab19da5c58a558f1c2c3dd1d4107559b
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2020-04-04 07:56:16 +0200

    Add a default error message

--- src/lib/responseHandler.js
+++ src/lib/responseHandler.js
@@ -1,23 +1,24 @@
 var handler = {
   handleError: function (err, unauthorized) {
-    console.log(err.response);
-    if(err.response.status === 401) {
-      return unauthorized;
-    } else if (err.response.status === 500) {
-      return "An unexcpected error occured!";
-    } else if (err.response.status === 400){
-      if (err.response.data.errors.email !== undefined) {
-        return "Please enter a valid email! 🤓";
-      } else if (err.response.data.errors.username !== undefined){
-        return "Invalid username";
+      if (err === undefined || err.response === undefined) {
+          return "Something has gone wrong! 😬";
+      } else if (err.response.status === 401) {
+          return unauthorized;
+      } else if (err.response.status === 500) {
+          return "An unexcpected error occured!";
+      } else if (err.response.status === 400){
+          if (err.response.data.errors.email !== undefined) {
+              return "Please enter a valid email! 🤓";
+          } else if (err.response.data.errors.username !== undefined){
+              return "Invalid username";
+          } else {
+              return err.response.data.errors;
+          }
+      } else if (err.response.status === 409) {
+          return "You are already tracking that habit! 🤷🏻‍♂️";
       } else {
-        return err.response.data.errors;
+          return err.response.data.error;
       }
-    } else if (err.response.status === 409) {
-        return "You are already tracking that habit! 🤷🏻‍♂️";
-    } else {
-      return err.response.data.error;
-    }
   }
 }
 export {handler as default};
